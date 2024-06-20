44f09eac786a3d2decd9767d83f0c25d950fb298
Pramod1818 <pk1818202@gmail.com>
2024-06-21 03:33:02 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="pramod",
+    # author_email="cloud@ineuron.ai",
+    packages=find_packages(),
+    install_requires=[],
+)
+
